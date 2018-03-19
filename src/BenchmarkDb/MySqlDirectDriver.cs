5bd685196aa4a30cb0d1bc65133ef8d11084f96b
Bradley Grainger <bradley.grainger@logos.com>
2018-03-19 15:49:06 -0700

    Add prepared statements to MySQL direct driver.

--- src/BenchmarkDb/MySqlDirectDriver.cs
+++ src/BenchmarkDb/MySqlDirectDriver.cs
@@ -9,50 +9,51 @@ using MySqlConnector.Direct;
 
 namespace BenchmarkDb
 {
-    public sealed class MySqlDirectDriver : DriverBase, IDisposable
-    {
-	    string _connectionString;
+	public sealed class MySqlDirectDriver : DriverBase, IDisposable
+	{
+		string _connectionString;
 
-        public override Func<Task> TryGetVariation(string variationName)
-        {
-            switch (variationName)
-            {
-                case Variation.AsyncCaching:
-                    return DoWorkAsyncCaching;
-                default:
-                    return NotSupportedVariation;
-            }
-        }
+		public override Func<Task> TryGetVariation(string variationName)
+		{
+			switch (variationName)
+			{
+				case Variation.AsyncCaching:
+					return DoWorkAsyncCaching;
+				default:
+					return NotSupportedVariation;
+			}
+		}
 
-        public override void Initialize(string connectionString, int threadCount)
-        {
-	        _connectionString = connectionString;
-        }
-		
-        public override async Task DoWorkAsyncCaching()
-        {
-	        var csb = new MySqlConnectionStringBuilder(_connectionString);
-	        var session = new MySqlSession(csb.Server, (int) csb.Port, csb.UserID, csb.Password, csb.Database);
+		public override void Initialize(string connectionString, int threadCount)
+		{
+			_connectionString = connectionString;
+		}
+
+		public override async Task DoWorkAsyncCaching()
+		{
+			var csb = new MySqlConnectionStringBuilder(_connectionString);
+			var session = new MySqlSession(csb.Server, (int) csb.Port, csb.UserID, csb.Password, csb.Database);
 
 			await session.ConnectAsync();
+			var statementId = await session.PrepareAsync(Program.TestQuery);
 
-            while (Program.IsRunning)
-            {
-                var results = new List<Fortune>();
+			while (Program.IsRunning)
+			{
+				var results = new List<Fortune>();
 
-                await session.ExecuteAsync(Program.TestQuery);
+				await session.ExecuteAsync(statementId);
 
-	            while (await session.ReadAsync())
-	            {
-					results.Add(new Fortune() { Id = session.ReadInt32(), Message = session.ReadString() });
-	            }
+				while (await session.ReadBinaryAsync())
+				{
+					results.Add(new Fortune() { Id = session.ReadInt32Binary(), Message = session.ReadString() });
+				}
 
-                Program.IncrementCounter();
-            }
-        }
+				Program.IncrementCounter();
+			}
+		}
 
-        public void Dispose()
-        {
-        }
-    }
+		public void Dispose()
+		{
+		}
+	}
 }
